4af181e15683469b9f81fdc1b2e202ea50c20a56
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2018-12-31 20:10:39 +0100

    Working router

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,15 @@
 
 <script>
 import Header from "./components/Header.vue";
-import Home from "./components/Home.vue";
 
 export default {
   name: "app",
   components: {
-    Header,
-    Home
+    Header
   },
   data() {
     return {
-      current_route: "Home"
+      current_route: this.$route.name
     };
   }
 };
--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import VueRouter from 'vue-router';
+import Home from './components/Home.vue';
+
+const routes = [{ path: '/', component: Home, name: 'Home' }];
+const router = new VueRouter({
+  routes
+});
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app');
