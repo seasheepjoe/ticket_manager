0b89a2b3017a52f72413f8cfc3e45ac62ce3b594
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-02 17:43:09 +0100

    Added login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
-    <h2>LOGIN</h2>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group id="group-email" label="Email :" label-for="email-input">
+        <b-form-input
+          id="email-input"
+          type="email"
+          v-model="login.email"
+          required
+          placeholder="john.doe@gmail.com"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="group-password" label="Password :" label-for="password-input">
+        <b-form-input
+          id="password-input"
+          type="password"
+          v-model="login.password"
+          required
+          placeholder="thebestpassword"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Login</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      login: {
+        email: "",
+        password: ""
+      }
+    };
+  }
 };
 </script>
 
