93c37305339598941339c3f4e33c3949291932e7
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-08 14:32:51 +0100

    Added interceptors on request and response to handle api token

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,9 +1,31 @@
 import axios from 'axios';
-//import store from '../store/index';
+import store from '../store/index';
 
 let api = axios.create();
 
-//api.defaults.headers.common['X-AUTH-TOKEN'] = store.state.API_TOKEN;
+api.interceptors.request.use(
+  function(config) {
+    let token = store.state.API_TOKEN;
+    if (token) config.headers['X-AUTH-TOKEN'] = token;
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  function(config) {
+    let token = store.state.API_TOKEN;
+    if (token) config.headers['X-AUTH-TOKEN'] = token;
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
+
 api.defaults.headers.common['Content-Type'] = 'application/json';
 api.defaults.headers.common['Accept'] = 'application/json';
 
