fa1afdaea4d0af6717f20a3357e176b2177f3e18
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-08 14:32:19 +0100

    Added function to get user tickets

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
     <h2>Home</h2>
+    <ul :key="index" v-for="(item, index) in tickets">
+      <li>
+        {{ item.title }}
+        <a>{{ item.author.fullname }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import api from "../config/ApiConfig";
+
 export default {
-  name: "Home"
+  mounted() {
+    this.$store.state.API_TOKEN !== null ? this.getTickets() : null;
+  },
+  name: "Home",
+  data() {
+    return {
+      tickets: []
+    };
+  },
+  methods: {
+    getTickets() {
+      let that = this;
+      api
+        .get("http://api.ticketmanager.com/tickets")
+        .then(function(response) {
+          let data = response.data;
+          console.log(data);
+          switch (data.status) {
+            case "success":
+              that.$data.tickets = data.tickets;
+            case "error":
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
 
