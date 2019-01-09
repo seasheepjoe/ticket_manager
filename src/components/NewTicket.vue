8efe0ec1ebec2ddbc8b9826d08780f2c43717961
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-09 13:05:15 +0100

    Added function to create ticket

--- src/components/NewTicket.vue
+++ src/components/NewTicket.vue
@@ -1,11 +1,60 @@
 <template>
-    
+  <b-form @submit.prevent="createTicket">
+    <b-form-group id="group-title" label="Title :" label-for="title-input">
+      <b-form-input
+        id="title-input"
+        type="text"
+        v-model="ticket.title"
+        required
+        placeholder="Nice title"
+      ></b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="primary">Create</b-button>
+    <div id="invalid-feedback" v-if="error">{{ error_message }}</div>
+  </b-form>
 </template>
 
 <script>
+import api from "../config/ApiConfig";
+
 export default {
-    name: "NewTicket"
-}
+  name: "NewTicket",
+  data() {
+    return {
+      ticket: {
+        title: ""
+      },
+      error: false,
+      error_message: ""
+    };
+  },
+  methods: {
+    createTicket() {
+      let that = this;
+      api
+        .post("http://api.ticketmanager.com/tickets/new", {
+          ticket_title: this.$data.ticket.title
+        })
+        .then(response => {
+          const { data } = response;
+          switch (data.status) {
+            case "success":
+              that.$router.push("/");
+              break;
+            case "error":
+              that.$data.error = true;
+              that.$data.error_message = that.$i18n.t(data.message);
+              break;
+            default:
+              break;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
 
 <style></style>
