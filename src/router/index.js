6730489b4ac9347286287ce23d57d143e3bf7c18
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-10 16:50:45 +0100

    Added function to set admin on route change

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,14 @@ import Login from '@/components/Login.vue';
 import Register from '@/components/Register.vue';
 import TicketDetails from '@/components/TicketDetails.vue';
 import NewTicket from '@/components/NewTicket.vue';
+import api from '../config/ApiConfig';
+import store from '../store/index';
+
+/* eslint-disable */
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -38,3 +42,31 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' || to.name !== 'Register') {
+    api
+      .get('http://api.ticketmanager.com/auth/check')
+      .then(response => {
+        const { data } = response;
+        switch (data.status) {
+          case 'success':
+            store.dispatch('setApiToken', data.user.apiToken);
+            store.dispatch('setUser', data.user);
+            next();
+            break;
+          case 'error':
+            next({ name: 'Login' });
+            break;
+          default:
+            break;
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  next();
+});
+
+export default router;
