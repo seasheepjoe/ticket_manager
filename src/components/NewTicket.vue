5f74e06e4a7cf38235cf88640f778657b4a904cb
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-10 21:32:00 +0100

    Added input to create message

--- src/components/NewTicket.vue
+++ src/components/NewTicket.vue
@@ -9,6 +9,15 @@
         placeholder="Nice title"
       ></b-form-input>
     </b-form-group>
+    <b-form-group id="group-title" label="Title :" label-for="title-input">
+      <b-form-input
+        id="content-input"
+        type="text"
+        v-model="ticket.message"
+        required
+        placeholder="Nice message"
+      ></b-form-input>
+    </b-form-group>
     <b-button type="submit" variant="primary">Create</b-button>
     <div id="invalid-feedback" v-if="error">{{ error_message }}</div>
   </b-form>
@@ -22,7 +31,8 @@ export default {
   data() {
     return {
       ticket: {
-        title: ""
+        title: "",
+        message: ""
       },
       error: false,
       error_message: ""
@@ -33,12 +43,14 @@ export default {
       let that = this;
       api
         .post("http://api.ticketmanager.com/tickets/new", {
-          ticket_title: this.$data.ticket.title
+          ticket_title: this.$data.ticket.title,
+          ticket_message: this.$data.ticket.message
         })
         .then(response => {
           const { data } = response;
           switch (data.status) {
             case "success":
+              console.log(data);
               that.$router.push("/");
               break;
             case "error":
